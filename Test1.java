94ff66848a02f0d263160795d9cd3a6ae603618b
heosungma <128379345+heosungma@users.noreply.github.com>
2023-03-20 21:16:41 +0900

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,25 @@
+package variable;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello World");
+        System.out.println("안녕하세요");
+        System.out.println(12);
+        System.out.println(-34);
+        System.out.println(3.14);
+        System.out.println(true);
+        System.out.println(false);
+        
+        System.out.println(123 + 345);
+        
+           
+	}
+
+	private static void printin(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
